0170fd785aec78216636cba9f9ce79269ac64092
Kirill Zhernov <ZhernovKR@yandex.ru>
2025-06-08 22:55:09 +0300

    New version for 5.11. Will test this later.

--- drv.c
+++ drv.c
@@ -1,12 +1,10 @@
-#include <linux/init.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/blkdev.h>
 #include <linux/blk-mq.h>
-#include <linux/list.h>
-// #include <linux/fs.h>
+#include <linux/genhd.h>
 
 #define MY_BLKDEV_NAME  "zhernov_blk_6"
-#define MY_BLK_SECT     (8 * 1024)
+#define NR_SECTORS      512
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("Zhernov Kirill");
@@ -45,36 +43,103 @@ struct task_struck + list_head + atomic + mutex
 
 */
 
-static int my_block_open(struct gendisk *disk, blk_mode_t mode)
+// struct blk_mq_tag_set tag_set;
+// struct request_queue *queue;
+// struct gendisk *disk;
+// struct block_device_operations dops = {
+    // .owner = THIS_MODULE,
+    // .open = my_block_open,
+    // .release = my_block_release
+// };
+
+static int block_major; // Number will be allocated after calling register_blkdev
+
+static int my_block_open(struct block_device *bdev, fmode_t mode)
 {
+    printk(MY_BLKDEV_NAME ": Device opened\n");
     return 0;
 }
 
-static void my_block_release(struct gendisk *gd)
+static void my_block_release(struct gendisk *gd, fmode_t mode)
 {
-    return;
+    printk(MY_BLKDEV_NAME ": Device released\n");
 }
 
-static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx, const struct blk_mq_queue_data *bd)
+/*
+*   Main structure
+*/
+static struct my_device {
+    // spinlock_t lock;     I guess we don't need that
+    struct blk_mq_tag_set tag_set;
+    struct request_queue *queue;
+    struct gendisk *gd;
+} dev;
+
+static blk_status_t my_queue_rq(struct blk_mq_hw_ctx *hctx, const struct blk_mq_queue_data *bd)
 {
+    blk_mq_end_request(bd->rq, BLK_STS_OK);
     return BLK_STS_OK;
 }
 
-struct blk_mq_tag_set tag_set;
-struct request_queue *queue;
-struct gendisk *disk;
-struct block_device_operations dops = {
+static const struct blk_mq_ops my_queue_ops = {
+    .queue_rq = my_queue_rq,
+};
+
+// Block device ops
+struct block_device_operations my_block_ops = {
     .owner = THIS_MODULE,
     .open = my_block_open,
     .release = my_block_release
 };
-static struct blk_mq_ops my_queue_ops = {
-   .queue_rq = my_block_request
-};
 
+/*
+*   Init my_device
+*/
+static int create_block_device(struct my_device *dev)
+{
+    dev->tag_set.ops = &my_queue_ops;
+    dev->tag_set.nr_hw_queues = 1;
+    dev->tag_set.queue_depth = 128;
+    dev->tag_set.numa_node = NUMA_NO_NODE;
+    dev->tag_set.cmd_size = 0;
+    dev->tag_set.flags = BLK_MQ_F_SHOULD_MERGE;
 
-static int block_major; // Number will be allocated after calling register_blkdev
+    int err = blk_mq_alloc_tag_set(&dev->tag_set);
+    if (err) {
+        printk(KERN_NOTICE "Failed to allocate tag set\n");
+        return -ENOMEM;
+    }
 
+    dev->queue = blk_mq_init_queue(&dev->tag_set);
+    if (IS_ERR(dev->queue)) {
+        blk_mq_free_tag_set(&dev->tag_set);
+        return -ENOMEM;
+    }
+
+    // dev->queue = blk_init_
+    dev->gd = alloc_disk(1);
+    if (!dev->gd) {
+        // blk_cleanup_queue(my_device.queue);
+        printk(KERN_NOTICE "Alloc disk failed\n");
+        unregister_blkdev(block_major, MY_BLKDEV_NAME);
+        return -ENOMEM;
+    }
+
+    dev->gd->major          = block_major;
+    dev->gd->first_minor    = 0;
+    dev->gd->fops           = &my_block_ops;
+    dev->gd->queue          = dev->queue;
+    // dev->gd->flags          = GENHD_FL_NO_PART_SCAN;
+    dev->gd->private_data   = dev;
+    strcpy(dev->gd->disk_name, MY_BLKDEV_NAME);
+    set_capacity(dev->gd, NR_SECTORS);
+
+    add_disk(dev->gd);
+
+    return 0;
+}
+
+// Register device
 static int __init drv_block_init(void)
 {
     block_major = register_blkdev(block_major, MY_BLKDEV_NAME);
@@ -83,85 +148,35 @@ static int __init drv_block_init(void)
         return -EBUSY;
     }
 
-    disk = blk_alloc_disk(1);
-    if (IS_ERR(disk)) {
-        printk(KERN_ERR "Alloc disk failed: %ld\n", PTR_ERR(disk));
-        unregister_blkdev(block_major, MY_BLKDEV_NAME);
-        return -ENOMEM;
-    }
-
-    disk->major = block_major;
-    disk->first_minor = 0;
-    disk->minors = 1;
-    disk->flags = GENHD_FL_NO_PART;
-    disk->fops = &dops;
-    strscpy(disk->disk_name, "blkdrv", sizeof(disk->disk_name));
-    set_capacity(disk, MY_BLK_SECT);
-
-    memset(&tag_set, 0, sizeof(tag_set));
-    tag_set.ops = &my_queue_ops;
-    tag_set.nr_hw_queues = 1;
-    tag_set.queue_depth = 128;
-    tag_set.numa_node = NUMA_NO_NODE;
-    tag_set.cmd_size = 0;
-    tag_set.flags = BLK_MQ_F_SHOULD_MERGE;
-
-    int err = blk_mq_alloc_tag_set(&tag_set);
-    if (err) {
-        printk(KERN_ERR "Failed to allocate tag set\n");
-        unregister_blkdev(block_major, MY_BLKDEV_NAME);
-        return -ENOMEM;
-    }
-
-    queue = blk_mq_init_queue(&tag_set);
-
-    if (IS_ERR(queue)) {
-        blk_mq_free_tag_set(&tag_set);
-        put_disk(disk);
-        unregister_blkdev(block_major, MY_BLKDEV_NAME);
-        return PTR_ERR(queue);
-    }
-
-    blk_queue_logical_block_size(queue, 512);
-    blk_queue_physical_block_size(queue, 512);
-
-    disk->queue = queue;
-
-    err = add_disk(disk);
-    if (err) {
-        printk(KERN_ERR "add_disk failed: %d\n", err);
-        blk_mq_destroy_queue(queue);
-        blk_mq_free_tag_set(&tag_set);
-        put_disk(disk);
-        unregister_blkdev(block_major, MY_BLKDEV_NAME);
-        return err;
-    }
+    int status = create_block_device(&dev);
+    if (status < 0)
+        return status;
 
     printk("Test module initialized: %d\n", block_major);
     return 0;
 }
 
+// Delete my_device
+static void delete_block_device(struct my_device *dev)
+{
+    if (dev->gd) {
+        del_gendisk(dev->gd);
+        put_disk(dev->gd);
+    }
+    blk_cleanup_queue(dev->queue);
+    blk_mq_free_tag_set(&dev->tag_set);
+}
+
+// Unregister block device
 static void __exit drv_block_exit(void)
 {
-    if (disk) {
-        del_gendisk(disk);
+    printk(MY_BLKDEV_NAME ": Block exit begin\n");
 
-        if (queue) {
-            blk_mq_destroy_queue(queue);
-            queue = NULL;
-        }
+    delete_block_device(&dev);
 
-        blk_mq_free_tag_set(&tag_set);
-        put_disk(disk);
-        disk = NULL;
-    }
+    unregister_blkdev(block_major, MY_BLKDEV_NAME);
 
-    if (block_major > 0) {
-        unregister_blkdev(block_major, MY_BLKDEV_NAME);
-        block_major = 0;
-    }
-
-    printk("Test module exit called\n");
+    printk(MY_BLKDEV_NAME ": Block exit end\n");
 }
 
 module_init(drv_block_init);
